f79ea7a503b417e47d18b3aa929f50d0c484958a
frone <mrfronne@gmail.com>
2019-08-14 20:59:14 +0300

    added database loading

--- elements/header.php
+++ elements/header.php
@@ -1,8 +1,8 @@
-<div class="menu">
+<nav class="menu">
 	
     <ul>
-    	<li><a>web-articles</a></li>
-		<li><a href="index.php?page=main"><i class="fa fa-home"></i>Main</a></li>
+    	<li><a align="center">web-articles</a></li>
+		<li><a href="index.php?page=main">Main</a></li>
 		<li><a href="index.php?page=info">Information</a></li>
 	</ul>
-</div>
+</nav>
--- pages/main.php
+++ pages/main.php
@@ -1,1 +1,33 @@
-�������
+<div class="maincontent">
+	<ul>
+		<? 
+			$connect = mysql_connect(localhost, root, '');
+			$select_db = mysql_select_db('articles web-site');
+
+			mysql_query("SET NAMES 'cp1251'");
+
+			$select = mysql_query("SELECT * FROM articles");
+
+			while($result = mysql_fetch_array($select))
+			{
+				echo "
+
+				<li>
+					<img src='$result[previewimage]'></br>
+					<h4 align='center'>$result[title]</h4></br>
+					<p align='justify'>$result[content]</p></br>
+					<nav align='right' style='margin-right: 10px;margin-bottom: 10px;'><button>���������</button></nav>
+				</li>
+				";
+			}
+
+		?>
+		<li>
+			1</br>
+			<img src="images/img1.jpg"></br>
+			<h4 align="center">Горы 1</h4></br>
+			<p align='justify'>Оооочень много текставыфовфлдывлывыфлдвоыфлдвоыфлдвоы флдыфвфывыфвфвыффвфыовлдф</p></br>
+			<nav align='right' style='margin-right: 10px;margin-bottom: 10px;'><button>Кнопка</button></nav>
+		</li>
+	</ul>
+</div>
